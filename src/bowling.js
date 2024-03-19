2eb23043342f623aeccdc8cbf9217f309731f50f
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-03-18 22:45:03 -0700

    Se agrega funcionalidad y se hace refactorizacion a la clase Game para calcular el puntaje con un strike

--- src/bowling.js
+++ src/bowling.js
@@ -1,6 +1,6 @@
 class Game {
     constructor() {
-      this.rolls = new Array(21).fill(0);
+      this.rolls = new Array(21).fill(0); // Crear un array de 21 posiciones llenas de 0
       this.currentRoll = 0;
     }
   
@@ -12,20 +12,39 @@ class Game {
       let score = 0;
       let frameIndex = 0;
       for (let frame = 0; frame < 10; frame++) {
-        if (this.isSpare(frameIndex)) {
-          score += 10 + this.rolls[frameIndex + 2];
+        if (this.isStrike(frameIndex)) {
+          score += 10 + this.strikeBonus(frameIndex);
+          frameIndex++;
+        } else if (this.isSpare(frameIndex)) {
+          score += 10 + this.spareBonus(frameIndex);
           frameIndex += 2;
         } else {
-          score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+          score += this.sumOfBallsInFrame(frameIndex);
           frameIndex += 2;
         }
       }
       return score;
     }
   
+    isStrike(frameIndex) {
+      return this.rolls[frameIndex] === 10;
+    }
+  
+    sumOfBallsInFrame(frameIndex) {
+      return this.rolls[frameIndex] + this.rolls[frameIndex+1];
+    }
+  
+    spareBonus(frameIndex) {
+      return this.rolls[frameIndex+2];
+    }
+  
+    strikeBonus(frameIndex) {
+      return this.rolls[frameIndex+1] + this.rolls[frameIndex+2];
+    }
+  
     isSpare(frameIndex) {
-      return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
+      return this.rolls[frameIndex] + this.rolls[frameIndex+1] === 10;
     }
   }
   
-  export default Game; 
+  export default Game;  
--- src/bowling.spec.js
+++ src/bowling.spec.js
@@ -13,6 +13,15 @@ describe("Bowling", () => {
         }
     };
 
+    const rollSpare = () => {
+        game.roll(5);
+        game.roll(5);
+    };
+
+    const rollStrike = () => {
+        game.roll(10);
+    };
+
     it("deberia calcular el puntaje de un juego sin puntos", () => {
       rollMany(20,0);
       expect(game.getScore()).toEqual(0);
@@ -21,7 +30,7 @@ describe("Bowling", () => {
     it("deberia calcular el puntaje con todos los lanzamientos de un punto", () => {
         rollMany(20,1);
         expect(game.getScore()).toEqual(20);
-      });
+    });
 
     it("deberia calcular el puntaje con un spare", () => {
         rollSpare();
@@ -30,8 +39,11 @@ describe("Bowling", () => {
         expect(game.getScore()).toEqual(16);
     });
 
-    const rollSpare = () => {
-        game.roll(5);
-        game.roll(5);
-    };
+    it("deberia calcular el puntaje con un strike", () => {
+        rollStrike();
+        game.roll(3);
+        game.roll(4);
+        rollMany(16, 0);
+        expect(game.getScore()).toEqual(24);
+    });
 });
